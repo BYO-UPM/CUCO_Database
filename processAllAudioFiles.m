bb4e957ed1f0f03f9a2404e1b8a0db400f9bef13
aguerrerolopez <alexjorguer@tsc.uc3m.es>
2023-11-29 15:52:39 +0100

    cleaning code

--- processAllAudioFiles.m
+++ processAllAudioFiles.m
@@ -2,7 +2,7 @@
 % Script to launch the karma_byo algorithm to extract formants and their trajectories from all WAV files in a directory
 function processAllAudioFiles(audioDir, numFormants, numAntiF, aParams, cepOrder, cepType, algFlag)
     % Add any required paths
-    addpath(genpath('/home/alexjorguer/GitHub/CUCOdb/karma_formantes_2.1'));
+    addpath(genpath('./GitHub/CUCOdb/karma_formantes_2.1'));
 
     % Find all WAV files in the specified directory and its subdirectories
     wavFiles = dir(fullfile(audioDir, '**', '*.wav'));
@@ -17,14 +17,14 @@ function processAllAudioFiles(audioDir, numFormants, numAntiF, aParams, cepOrder
         pngFile = strrep(audioFile, '.wav', '.png');
         matFile = strrep(audioFile, '.wav', '.mat');
         
-        if exist(pngFile, 'file') && exist(matFile, 'file')
-            disp('Skipping: .png and .mat files already exist.');
-        else
+        %if exist(pngFile, 'file') && exist(matFile, 'file')
+        %    disp('Skipping: .png and .mat files already exist.');
+        %else
             % Call the processAudio function for each file
-            processAudio(audioFile, numFormants, numAntiF, aParams, cepOrder, cepType, algFlag);
+        processAudio(audioFile, numFormants, numAntiF, aParams, cepOrder, cepType, algFlag);
             
-            disp(['Processing completed for: ', audioFile]);
-        end
+        disp(['Processing completed for: ', audioFile]);
+        %end
     end
 end
 
